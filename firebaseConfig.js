7dfba188e01d74c1323193be759d83f1a74e20cc
yaninacba <126228201+yaninacba@users.noreply.github.com>
2024-04-03 14:25:54 -0300

    ll

---
+++ firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDukGl-6c2HfKaKie0FGJzWGeCBF-5hjsU",
+  authDomain: "kambio-67f17.firebaseapp.com",
+  projectId: "kambio-67f17",
+  storageBucket: "kambio-67f17.appspot.com",
+  messagingSenderId: "205221704558",
+  appId: "1:205221704558:web:191ca7e10328d6abb9077f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
