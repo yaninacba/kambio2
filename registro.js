9d548c874b8c375ea0a9a7fb8554ae242d816a93
yaninacba <126228201+yaninacba@users.noreply.github.com>
2024-04-10 14:21:08 -0300

    v2

--- registro.js
+++ registro.js
@@ -1,37 +1,23 @@
-// Importa la instancia de la aplicación Firebase
-import app from "./firebaseConfig";
+import { initializeApp } from "firebase/app";
+import { addDoc, getFirestore } from "firebase/firestore";
 
-// Ahora puedes usar la instancia de la aplicación Firebase donde la necesites en este archivo
-// Por ejemplo, si necesitas acceder a la base de datos Firestore, puedes hacerlo aquí
-
-const db = getFirestore(app);
-
-// Esta función se ejecutará cuando se haga clic en el botón "Guardar"
-button.addEvenListener("click", guardar)
-
-function guardar() {
-    // Obtener los valores de los campos de texto
-    const nombre = document.getElementById("nombre").value;
-    const apellido = document.getElementById("apellido").value;
-    const telefono = document.getElementById("telefono").value;
-
-    // Verificar si los campos están vacíos
-    if (nombre === '' || apellido === '' || telefono === '') {
-        alert("Por favor, complete todos los campos");
-        return; // Salir de la función si algún campo está vacío
-    }
-
-    // Agregar el documento a la colección "Usuario" en Firestore
-    addDoc(collection(db, "Usuario"), {
-        Nombre: nombre,
-        Apellido: apellido,
-        Telefono: telefono
-    })
-    .then(() => {
-        alert("Registrado exitosamente");
-    })
-    .catch((error) => {
-        alert("No pudimos registrar la información, intenta nuevamente");
-        console.error("Error al guardar documento: ", error);
+const firebaseConfig = initializeApp( {
+    apiKey: "AIzaSyDlLngo2i5bBqXrF7uWh9Rk7LBetyvHth0",
+    authDomain: "boris-f276f.firebaseapp.com",
+    projectId: "boris-f276f",
+    storageBucket: "boris-f276f.appspot.com",
+    messagingSenderId: "277241048439",
+    appId: "1:277241048439:web:c3c73d6263c6eeb5204572",
+    measurementId: "G-0EJS7NMRLK"
+  });
+  const db = getFirestore(app);
+  console.log('hola probando');
+  const usuarioColec = collection(db, "usuario");
+ 
+  async function agregarDoumento(){
+    const nuevoDoc= addDoc(usuarioColec, {
+        nombre: document.getElementById('nombre');
+        apellido: document.getElementById('apellido');
+        telefono: document.getElementById('telefono');
     });
-}
+  }
