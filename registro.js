15acc446072e77250cb18ac4195719f40554b9ec
yaninacba <126228201+yaninacba@users.noreply.github.com>
2024-04-11 01:43:16 -0300

    33999

--- registro.js
+++ registro.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 
-const firebaseConfig = initializeApp( {
-    apiKey: "AIzaSyDlLngo2i5bBqXrF7uWh9Rk7LBetyvHth0",
-    authDomain: "boris-f276f.firebaseapp.com",
-    projectId: "boris-f276f",
-    storageBucket: "boris-f276f.appspot.com",
-    messagingSenderId: "277241048439",
-    appId: "1:277241048439:web:c3c73d6263c6eeb5204572",
-    measurementId: "G-0EJS7NMRLK"
-  });
+const firebaseConfig = {
+  apiKey: "AIzaSyDlLngo2i5bBqXrF7uWh9Rk7LBetyvHth0",
+  authDomain: "boris-f276f.firebaseapp.com",
+  projectId: "boris-f276f",
+  storageBucket: "boris-f276f.appspot.com",
+  messagingSenderId: "277241048439",
+  appId: "1:277241048439:web:c3c73d6263c6eeb5204572",
+  measurementId: "G-0EJS7NMRLK"
+};
   const app = initializeApp(firebaseConfig);
   const db = getFirestore(app);
   console.log('hola probando');
