2cde9db0c1c82c9e9247c2328f385087101d2d3c
yaninacba <126228201+yaninacba@users.noreply.github.com>
2024-04-17 01:11:43 -0300

    ju9911jk

--- buscar.js
+++ buscar.js
@@ -1,7 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
 import { collection, query, where, getDocs, getFirestore} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDlLngo2i5bBqXrF7uWh9Rk7LBetyvHth0",
+    authDomain: "boris-f276f.firebaseapp.com",
+    projectId: "boris-f276f",
+    storageBucket: "boris-f276f.appspot.com",
+    messagingSenderId: "277241048439",
+    appId: "1:277241048439:web:c3c73d6263c6eeb5204572",
+    measurementId: "G-0EJS7NMRLK"
+  };
+  const app = initializeApp(firebaseConfig);
+ const db = getFirestore(app);
 
 
 //funcion para leer
@@ -21,6 +32,11 @@ async function mostrarDatos() {
       console.error("Error al recuperar datos:", error);
     }
   }
+
+
+
+
+
   
 
   
