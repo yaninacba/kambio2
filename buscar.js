5b9b77cefc7a3dc809ca473ba122dd17b4170696
yaninacba <126228201+yaninacba@users.noreply.github.com>
2024-05-22 03:42:14 -0300

    Update buscar.js
    98983

--- buscar.js
+++ buscar.js
@@ -60,8 +60,9 @@ document.getElementById("botonHfc").addEventListener("click", function() {
                         window.open(enlaceWhatsApp, '_blank');
                         
                         try {
+                            const eli= doc.id;
                             // Eliminar el documento de Firestore
-                            await deleteDoc(db, "usuario", doc.id);
+                            await deleteDoc(doc(db, "usuario", "eli"));
                             console.log("Documento eliminado con éxito");
                             
                             // Eliminar el div de datosContainer
