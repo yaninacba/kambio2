35062fecb3de563b4a629b67a0ec6149bad51af0
yaninacba <126228201+yaninacba@users.noreply.github.com>
2024-05-22 02:51:40 -0300

    Update buscar.js
    ccccccccccccccccc

--- buscar.js
+++ buscar.js
@@ -27,7 +27,7 @@ document.getElementById("botonHfc").addEventListener("click", function() {
        
 
         const q = query(collection(db, "usuario"), where("isla", "==", "hfc"));
-        const querySnapshot = await q.get();
+        const querySnapshot = await getDocs(q);
 
         const datosContainer = document.getElementById('datosContainer');
         datosContainer.innerHTML = ''; // Limpiar contenido previo
