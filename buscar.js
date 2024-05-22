f92e7c345989285bd48d54905238427e5040a955
yaninacba <126228201+yaninacba@users.noreply.github.com>
2024-05-22 01:18:37 -0300

    Update buscar.js
    gsgg

--- buscar.js
+++ buscar.js
@@ -60,7 +60,7 @@ document.getElementById("botonHfc").addEventListener("click", function() {
                         // Si el usuario confirma el cambio, entonces proceder con el resto del código
                         try {
                           
-                           await deleteDoc(doc(db, "usuario",doc.id));
+                           await deleteDoc(doc(db, "usuario",doc.data().id));
                             console.log("Documento eliminado con éxito");
 
                             // Eliminar el div de datosContainer
